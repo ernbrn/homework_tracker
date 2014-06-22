a20d3082aec689393d433006cbe080ad58afb9e4
Erin Brown <emberbrown@gmail.com>
2014-06-22 14:09:38 -0400

    assignment description can read markdown

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @user = User.all
+    @assignments = Assignment.all
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def markdown(descriptiontext)
+    renderOptions = {hard_wrap: true, filter_html: true}
+    # markdownOptions = {autolink: true, no_intra_emphasis: true}
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(renderOptions))
+    markdown.render(descriptiontext).html_safe
+ end
 end
